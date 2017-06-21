842cc4aa18503ec18bc24b05460828f8c281a2b8
Brandi Anderson <branderso87@gmail.com>
2017-06-21 15:57:52 -0500

    task and completed tasks populate

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express()
+const mustache = require('mustache-express')
+const bodyParser = require('body-parser')
+
+app.use(express.static('public'))
+app.engine('mustache', mustache())
+app.set('view engine', 'mustache')
+app.use(bodyParser.urlencoded({ extended: false }))
+
+const todo = []
+const complete = []
+
+app.get('/', function (req, res) {
+  res.render('todo', {todo: todo, complete})
+})
+
+app.post('/', function (req, res) {
+  const task = req.body.task
+  const tocomplete = req.body.tocomplete
+  
+  if (task) {
+    todo.push(task)
+  } else if (tocomplete) {
+    todo.splice(todo.length - 1, 1)
+    complete.push(tocomplete)
+  }
+  res.redirect('/')
+})
+
+app.listen(3000, function () {
+  console.log('ok cool, listening!')
+})
